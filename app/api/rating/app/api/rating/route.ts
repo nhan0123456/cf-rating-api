68c31d379d4eb27189b95086de8dc73a7a4cb038
Your Name <nguyentrongnhan5109@gmail.com>
2025-06-12 17:36:08 +0700

    Fix: move route.ts to correct path

--- app/api/rating/app/api/rating/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/rating/route.ts
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  const res = await fetch('https://codeforces.com/api/user.info?handles=nhan0123456')
-  const data = await res.json()
-  return NextResponse.json(data)
-}
